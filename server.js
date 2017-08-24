d242b3ebaf508ad731fe8cdb93a69a2fb50a0111
Sakvin <sakvin01@gmail.com>
2017-08-24 14:05:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get ('article1', function(req, res){
+ res.sendFile(path.join(__dirname, 'article1'));
+});
+
+app.get ('article2', function(req, res){
+ res.sendFile(path.join(__dirname, 'article2'));
+});
+
+app.get ('article3', function(req, res){
+ res.sendFile(path.join(__dirname, 'article3'));
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
